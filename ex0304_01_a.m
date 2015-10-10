456fb94c17d1d45375f521e06a8ea167cd7b7471
minwoobae <minubae.nyc@gmail.com>
2015-10-10 16:36:43 -0400

    modified ex0304_01_a.m

--- ex0304_01_a.m
+++ ex0304_01_a.m
@@ -4,6 +4,15 @@
 % Date: Oct/10/2015
 % Author: Minwoo Bae
 
-function H = hermite(x, x_i, f_i, fp_i)
-n = length(2*x_i);
-H = zeros(n+1, n+1);
+function H=hermite(x, x_i, f_i, fp_i)
+n = length(x_i);
+z = zeros(1, 2*n);
+H = zeros(2*n, 2*n);
+
+for i=0:n
+    z(2*i) = x_i(i);
+    z(2*i+1) = x_i(i);
+    H(2*i, 0) = f_i(x_i(i));
+    H(2*i+1, 0) = f_i(x_i(i));
+    H(2*i+1, 0) = fp_i(x_i(i));
+end
