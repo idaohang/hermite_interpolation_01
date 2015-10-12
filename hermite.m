707a01201febe129e67ab807c5d6a8f431694bd3
minwoobae <minubae.nyc@gmail.com>
2015-10-11 21:53:46 -0400

    modified hermite algorithm

--- hermite.m
+++ hermite.m
@@ -6,36 +6,24 @@
 
 function H = hermite (x, y, yp)
 n = length(x)-1;
-z = zeros(1, 2*n);
-H = zeros(2*n, 2*n);
+z = zeros(1, 2*(n+1));
+H = zeros(2*(n+1), 2*(n+1));
 
-z(2*n,:) = x(:);
-z(2*n+1,:) = x(:);
-H(2*n,:) = y(:);
-H(2*n+1,:) = y(:);
-H(2*n+1,:,1) = yp(:);
-if n ~= 0
-    H(2*n,:,1) = (H(2*n,:) - H(2*n-1,:))/z(2*n,:)-z(2*n-1,:);
+for i=1:(n+1)
+    z(2*i+1) = x(i);
+    z(2*i+2) = x(i);
+    H(2*i+1,1) = y(i);
+    H(2*i+2,1) = y(i);
+    H(2*i+2,2) = yp(i);
+    if i ~= 1
+        H(2*i+1,2) = (H(2*i+1,1) - H(2*i,1))/(z(2*i+1)-z(2*i));
+    end;
 end;
 
-
-% n = length (x);
-% z = zeros (1, 2*n);
-% f = zeros (1, 2*n);
-% 
-% z(1:2:2*n-1) = x;
-% z(2:2:2*n)   = x;
-% f(1)         = y(1);
-% f(3:2:2*n-1) = ( y(2:n) - y(1:n-1) ) ./ ( x(2:n) - x(1:n-1) );
-% f(2:2:2*n)   = yp;
-% 
-% for i = 3:2*n
-%     f(i:2*n) = ( f(i:2*n) - f(i-1:2*n-1) ) ./ ( z(i:2*n) - z(1:2*n-i+1) );
-% end;
-% 
-% hp = zeros ( 1, 2*n );
-% p = [1];
-% for i = 1:2*n
-%     hp = hp + f(i) * [ zeros(1,2*n-i) p ];
-% 	p = conv ( p, [ 1 -z(i) ] );
-% end;
+for i=3:2*n+2
+    for j=3:i
+        disp('hello')
+        %fprintf('%d',H(i+1,j));
+        H(i,j)=(H(i+1,j)-H(i,j))/(z(i)-z(i-j+1));
+    end;
+end;
