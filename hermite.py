498cd21df01bf649a7d9d0099ba719117570dc57
Minu Bae <minubae.nyc@gmail.com>
2015-10-23 12:51:42 -0400

    Delete hermite.py

--- hermite.py
+++
@@ -1,9 +0,0 @@
-
-# Numerical Analysis (Math 328) at the City College of New York, CUNY
-# Chapter 3.4 in Numerical Analysis
-# Date: Oct/13/2015
-# Author: Minwoo Bae
-
-def hermite(x, y, yp):
-	H = 0*[]
-	return H
